203348fcdab8d44720ec00f10eb543c6fe869c37
neilbluejohn <111980378+neilbluejohn@users.noreply.github.com>
2024-05-29 18:37:15 +0100

    Update tt_um_njp_micro.v

--- src/tt_um_njp_micro.v
+++ src/tt_um_njp_micro.v
@@ -25,7 +25,7 @@ module tt_um_njp_micro (
   wire _unused = &{ena, clk, rst_n, 1'b0};
   
   wire sys_clk, sys_rst, flag, C0, C1, C2, C3, C4, C5, C6, C7, C8, C9, C10, C11, C12;
-  wire [7:0] MP;
+  wire [3:0] inputA;
   assign sys_rst = !rst_n;
   assign sys_clk = clk;
   assign inputA[3:0] = ui_in[3:0];
