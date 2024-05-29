facc0f890de5eac0ad3c97cc475ad62bbdbfdfac
neilbluejohn <111980378+neilbluejohn@users.noreply.github.com>
2024-05-29 16:51:13 +0100

    Update tt_um_njp_micro.v

--- src/tt_um_njp_micro.v
+++ src/tt_um_njp_micro.v
@@ -23,10 +23,6 @@ module tt_um_njp_micro (
 
   // List all unused inputs to prevent warnings
   wire _unused = &{ena, clk, rst_n, 1'b0};
-
-endmodule
-
-module tt_um_njp_micro(input sys_clk, nsys_rst, input [3:0] inputA, inputB, output [7:0] SMP_out);
   
   wire sys_rst, flag, C0, C1, C2, C3, C4, C5, C6, C7, C8, C9, C10, C11, C12;
   wire [3:0] A;
